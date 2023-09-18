db2f0761c02532f83603fb2fd88c6e98b180757e
Dmytro Lyshtva <lyshtva.dmytro@gmail.com>
2023-09-18 17:15:21 +0200

    Initial commit

---
+++ Friendly/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Friendly
+//
+//  Created by Dmytro Lyshtva on 18.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
